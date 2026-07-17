4e6f37e4a8a8743da080a62a08d4af4bb14c9858
2023371019 <2023371019@uteq.edu.mx>
2026-07-16 21:10:42 -0600

    actualización endpoint prestamos

--- ms-devoluciones/src/main/java/com/scrip/msdevoluciones/dto/DevolucionDto.java
+++ ms-devoluciones/src/main/java/com/scrip/msdevoluciones/dto/DevolucionDto.java
@@ -1,4 +1,23 @@
 package com.scrip.msdevoluciones.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DevolucionDto {
+    private UUID id;
+    private UUID prestamoId;
+    private UUID usuarioId;
+    private UUID libroId;
+    private OffsetDateTime fechaDevolucion;
+    private Boolean tardia;
+    private Integer diasRetraso;
 }
